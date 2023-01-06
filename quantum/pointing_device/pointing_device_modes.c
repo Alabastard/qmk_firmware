fdd390a8cab9d2c1d5cf56b6fc2d6b750ef2547c
Alabastard-64 <snipac+github@gmail.com>
2023-01-06 11:53:00 -0700

    real initial release everything working and tested on Atmel U4 chips

--- quantum/pointing_device/pointing_device_modes.c
+++ quantum/pointing_device/pointing_device_modes.c
@@ -459,6 +459,11 @@ static report_mouse_t process_pointing_mode(pointing_mode_t pointing_mode, repor
 
         // drag scroll mode (sets mouse axes to mouse_report h & v with divisor)
         case PM_DRAG:
+#    ifdef MOUSE_WHEEL_HIRES_ENABLE
+            if (resolution_multiplier & 1 << 0) pointing_mode.y *= MAX(MOUSE_WHEEL_MULTIPLIER/pointing_mode.divisor, 1);
+            if (resolution_multiplier & 1 << 2) pointing_mode.x *= MAX(MOUSE_WHEEL_MULTIPLIER/pointing_mode.divisor, 1);
+            if (resolution_multiplier) pointing_mode.divisor = 1;
+#    endif
             mouse_report.h = apply_divisor_hv(pointing_mode.x);
             pointing_mode.x -= multiply_divisor_hv(mouse_report.h);
             mouse_report.v = apply_divisor_hv(pointing_mode.y);
